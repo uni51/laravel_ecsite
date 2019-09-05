e392a615714f6cf0171a1d204e8c7d0c2817c6de
uni51 <uni51@users.noreply.github.com>
2019-09-06 06:31:24 +0900

    商品一覧を表示する

---
+++ resources/views/item/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-left">
+            @foreach ($items as $item)
+                <div class="col-md-4 mb-2">
+                    <div class="card">
+                        <div class="card-header">{{ $item->name }}</div>
+                        <div class="card-body">
+                            {{ $item->amount }}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="row justify-content-center">
+            {{ $items->links() }}
+        </div>
+    </div>
+@endsection
