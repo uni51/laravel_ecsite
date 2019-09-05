faaf74d5f6a96859d7454883e543dd7fe8763d9d
uni51 <uni51@users.noreply.github.com>
2019-09-06 06:38:01 +0900

    商品検索を実装する

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -12,9 +12,16 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $items = Item::paginate(15);
+        // リクエストパラメタにkeywordが入っていたら検索機能を動かす
+        if($request->has('keyword')) {
+            // SQLのlike句でitemsテーブルを検索する
+            $items = Item::where('name', 'like', '%'.$request->get('keyword').'%')->paginate(15);
+        }
+        else{
+            $items = Item::paginate(15);
+        }
         return view('item/index', ['items' => $items]);
     }
 
