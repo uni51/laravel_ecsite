acf164d38befc1a0422bec18130d5df4803c122d
uni51 <uni51@users.noreply.github.com>
2019-09-06 07:32:59 +0900

    カートの確認

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'ItemController@index');
 Route::get('/item/{item}', 'ItemController@show');
 Route::post('/cartitem', 'CartItemController@store');
+Route::get('/cartitem', 'CartItemController@index');
 
 Auth::routes();
 
